4c5bb4e0b505f44b23061cc39b0b3e616df520c5
fyr233 <36994804+fyr233@users.noreply.github.com>
2021-03-30 15:25:05 +0800

    单机

--- main-active.py
+++ main-active.py
@@ -1,3 +1,75 @@
 # coding:utf-8
 # main-active是主动端，负责解析midi、通过http发送按键事件给被动端（多个）
 # 主动端将监听热键，在游戏后台开始播放
+
+import mido
+import keyboard
+import requests
+from threading import Timer
+
+from config import config_active as config
+
+class Note:
+    def __init__(self, time, pitch, velocity, key_signature):
+        self.time = time
+        self.pitch = pitch
+        self.velocity = velocity
+        self.key_signature = key_signature
+
+Track = []
+
+def loadMidiFile():
+
+    key_signature = config['key_note']['G']
+    tempo = 500000
+    globaltime = 0.0
+
+    for filename in config['midifile']:
+        
+        mid = mido.MidiFile(filename)
+        print(mid.filename, mid.length, mid.ticks_per_beat)
+
+        for i, track in enumerate(mid.tracks):
+            print('Track', i, track.name)
+
+            starttime = globaltime
+            for msg in track:
+
+                globaltime += msg.time / mid.ticks_per_beat * tempo / 1000000
+                if msg.type == 'key_signature':
+                    key_signature = config['key_note'][msg.key]
+                elif msg.type == 'set_tempo':
+                    tempo = msg.tempo
+                elif msg.type == 'note_on':
+                    Track.append(Note(globaltime, msg.note, msg.velocity, key_signature))
+            
+            globaltime = starttime
+        
+        globaltime += 3.0
+    
+    Track.sort(key= lambda item: item.time)
+
+def play(index):
+    print(index)
+    url = 'http://' + config['passive_server'][0]['ip'] + ':' + config['passive_server'][0]['port'] + '/playnote'
+    r = requests.post(
+        url,
+        data={
+            'pitch': Track[index].pitch,
+            'key': Track[index].key_signature
+        }
+    )
+    t = Timer(
+        Track[index+1].time - Track[index].time,
+        play,
+        (index+1,)
+    )
+    t.start()
+
+def beginPlay():
+    play(0)
+
+if __name__ == '__main__':
+    loadMidiFile()
+
+    beginPlay()
--- main-passive.py
+++ main-passive.py
@@ -1,2 +1,35 @@
 # coding:utf-8
 # main-passive是被动端，负责通过http接收按键事件、模拟键盘输入
+
+import json
+import keyboard
+from flask import Flask, request
+
+from config import config_passive as config
+
+app = Flask(__name__)
+
+# key_note:"G"=55,"Ab"=56,"A"=57,"Bb"=58,"B"=59,"C"=60,"C#"=61,"D"=62,"D#"=63,"E"=64,"F"=65,"F#"=66
+def pitchToKey(v,key):
+    print((v - key + 12)%36)
+    note_tem = ''
+    
+    try:
+        note_tem = config['note'][(v - key + 12)%36]
+    except:
+        note_tem = config['note'][(v - key + 12 + 1)%36]
+    
+    return config['key'][note_tem]
+
+@app.route('/playnote', methods=['POST'])
+def playnote():
+    #print(request.form)
+    pitch = request.form['pitch']
+
+    key = pitchToKey(int(pitch), int(request.form['key']))
+    keyboard.press_and_release(key)
+
+    return 'Success'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True)
